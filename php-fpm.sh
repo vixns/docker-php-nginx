d8f72777195c85c80873e5ed804d457f0cb1e662
Stéphane Cottin <stephane.cottin@vixns.com>
2015-10-21 16:25:59 +0200

    better regex

--- php-fpm.sh
+++ php-fpm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 exec 1> /proc/1/fd/1
 exec 2> /proc/1/fd/2
-sed -i -e "s/\$PHP_USER/${PHP_USER}/" /usr/local/etc/php-fpm.d/*
+sed -i -e "s/^user.*=.*$/user = ${PHP_USER}/" /usr/local/etc/php-fpm.d/*
 exec php-fpm --force-stderr
