a8ff728f9f3959e1a564aec1bf0956835c8f09bd
Stéphane Cottin <stephane.cottin@vixns.com>
2018-05-25 22:01:14 +0200

    add haproxy sidecar

--- 7/runsvdir-start.sh
+++ 7/runsvdir-start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
 export PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin
-
-exec runsvdir -P /etc/service
+onterm() {
+  pkill -HUP runsvdir
+  exit $?
+}
+trap onterm TERM INT
+runsvdir -P ${SERVICEDIR:-/etc/service} & wait "$!"
