d0f606eab69005e1e116bfaee1be67c365108f3b
Stéphane Cottin <stephane.cottin@vixns.com>
2021-03-05 15:10:37 +0100

    when using mesos, use sandbox files for stdout/stderr

--- run.sh
+++ run.sh
@@ -3,7 +3,13 @@ exec 2>&1
 
 export PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin
 
-ln -sf /proc/$$/fd/1 /tmp/stdout
-ln -sf /proc/$$/fd/2 /tmp/stderr
+ln -sf /dev/stdout /tmp/stdout
+ln -sf /dev/stderr /tmp/stderr
+
+if [ -n "$MESOS_SANDBOX" ]
+then	
+	ln -sf ${MESOS_SANDBOX}/stdout /tmp/stdout
+	ln -sf ${MESOS_SANDBOX}/stderr /tmp/stderr
+fi
 
 exec runsvdir -P /etc/service
